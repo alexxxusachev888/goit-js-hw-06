387521a0935bffd0e7a16bb5709cd30c568970b3
alexxxusachev888 <alexxxusachev888@gmail.com>
2022-11-01 16:34:35 +0200

    changed dir for HW1

--- src/js/task-06.js
+++
@@ -1,9 +0,0 @@
-const inputElem = document.querySelector('#validation-input');
-
-inputElem.addEventListener('blur', () => {
-  if (inputElem.value.length == inputElem.dataset.length) {
-    inputElem.classList.add('valid');
-  } else {
-    inputElem.classList.add('invalid');
-  }
-});
--- src/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const inputRangeElem = document.querySelector('#font-size-control');
-const textElem = document.querySelector('#text');
-
-inputRangeElem.addEventListener('input', (event) => {
-  textElem.style.fontSize = event.currentTarget.value + 'px';
-});
